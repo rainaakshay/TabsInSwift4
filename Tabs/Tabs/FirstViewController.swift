42358c72193d8597c7673474f863435a92147cc0
Akshay Raina <araina1995@gmail.com>
2019-01-02 11:47:12 +0530

    Add files via upload

---
+++ Tabs/Tabs/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Tabs
+//
+//  Created by apple on 18/12/18.
+//  Copyright © 2018 Seraphic. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tabs/Tabs/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Tabs
+//
+//  Created by apple on 18/12/18.
+//  Copyright © 2018 Seraphic. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
